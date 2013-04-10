4b26d1da08f8c1ae76cf1a8b77e63b871069cc44
Nicholas Tietz <ntietz@gmail.com>
2013-04-10 16:28:07 -0400

    Added wrapper interface

--- src/capstone/wrapper/Wrapper.java
+++ src/capstone/wrapper/Wrapper.java
@@ -6,5 +6,20 @@ import java.util.*;
 public interface Wrapper
 {
     public List<ProgramError> prepare(String programText);
+    public void killDebugger();
+    public void runProgram();
+
+    public String getStdOut();
+    public void provideInput(String input);
+    public StackFrame getLocalValues();
+    public List<StackFrame> getStack();
+    public String evaluateExpression(String expression);
+
+    public void stepIn();
+    public void stepOut();
+    public void stepOver();
+
+    public void addBreakpoint(int lineNumber);
+    public int getLineNumber();
 }
 
